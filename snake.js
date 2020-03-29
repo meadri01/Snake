95bb42efb4a467e59ce7dadbb477bb9f1e60597f
Adri Mukherjee <meadri17@gmail.com>
2020-03-29 19:50:19 +0530

    small updates in UI

--- snake.js
+++ snake.js
@@ -1,15 +1,16 @@
-var scl = 10;
+var scl = 20;
 class Snake{
    constructor(){
         this.x=0;
         this.y=0;
         this.xspeed=1;
         this.yspeed=0;
+        this.tail=[];
    }    
 
     createSnake(){
 
-        fill(255);
+        fill(128,255,0);
         rect(this.x,this.y,scl,scl);
     }
     update(){
@@ -18,4 +19,5 @@ class Snake{
         
 
     }
+    
 }
