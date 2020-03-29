77055e92d69baecda15e131bb7fafb3f92f7e1fa
Adri Mukherjee <meadri17@gmail.com>
2020-03-29 16:37:52 +0530

    Added key movements

--- setup.js
+++ setup.js
@@ -18,9 +18,7 @@ function draw(){
     s.createSnake();
     s.update();
     createFood();
-    
-   
-    frameRate(50);
+    frameRate(10);
 }
 
 function createFood(){
@@ -28,4 +26,24 @@ function createFood(){
     console.log(random_x + ' ' + random_y);
     fill(255,102,102);
     rect(ceil(random_x),ceil(random_y),scl,scl);
+}
+
+function keyPressed(){
+    if(keyCode == LEFT_ARROW)
+    {
+        s.xspeed = -1;
+        s.yspeed = 0;
+    } else if(keyCode == RIGHT_ARROW)
+    {
+        s.xspeed = 1;
+        s.yspeed = 0;
+    } else if(keyCode == UP_ARROW)
+    {
+        s.xspeed = 0;
+        s.yspeed = -1;
+    } else if(keyCode == DOWN_ARROW)
+    {
+        s.xspeed = 0;
+        s.yspeed = 1;
+    }
 }
