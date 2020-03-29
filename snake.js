c938edd1bed66d2d6e37da114d6dd8f453bf30b3
Adri Mukherjee <meadri17@gmail.com>
2020-03-29 22:06:09 +0530

    tail lengthens

--- snake.js
+++ snake.js
@@ -10,10 +10,24 @@ class Snake{
 
     createSnake(){
 
+        for(var i=0; i<this.tail.length; i++){
+            fill(128,255,0);
+            rect(this.tail[i].x,this.tail[i].y,scl,scl);
+        } 
         fill(128,255,0);
         rect(this.x,this.y,scl,scl);
     }
     update(){
+        if(this.total == this.tail.length){
+            for(var i=0; i<this.tail.length; i++){
+                this.tail[i] = this.tail[i+1];
+            }
+            
+
+
+        }
+        this.tail[this.total-1] = createVector(this.x,this.y);
+
         this.x=this.x + this.xspeed * scl;
         this.y=this.y + this.yspeed * scl;
         
